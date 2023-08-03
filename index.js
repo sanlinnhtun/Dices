5940aef3f11ae1850d2cdb29951b6c324c2ec7be
albert16102002 <sanlynnh@gmail.com>
2023-08-03 22:59:04 +0630

    Change of dicee

---
+++ index.js
@@ -0,0 +1,43 @@
+// first random number
+
+let randomNumber1= Math.floor(Math.random() * 6 + 1);
+console.log("First random number is", randomNumber1);
+
+// second random number
+
+let randomNumber2= Math.floor(Math.random() * 6 + 1);
+console.log("Second random number is" , randomNumber2);
+
+// first random image
+
+const imageKey1="/images/dice"+ randomNumber1 + ".png";
+const firstImageTag = document.getElementsByClassName("img1")[0];
+firstImageTag.src=imageKey1;
+
+// second random image
+
+const imageKey2="/images/dice"+ randomNumber2 + ".png";
+const secondImageTag= document.getElementsByClassName("img2")[0];
+secondImageTag.src=imageKey2
+
+//click me
+function showAlert() {
+    alert(Date.now().toString)
+}
+
+const refreshTag= document.getElementById("Refresh");
+
+refreshTag.addEventListener("click",showAlert);
+
+
+//Decision statement
+
+const headElementTag=document.getElementById("headElement");
+
+if(randomNumber1>randomNumber2){
+    headElementTag.innerHTML="Player 1 win🎲🏅"
+}else if(randomNumber2>randomNumber1){
+    headElementTag.innerHTML="Player 2 win🎲🏅"
+}else {
+    headElementTag.innerHTML="Draw⛳"
+}
